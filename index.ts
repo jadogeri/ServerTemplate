aecd15bbbd0fc2a15d0c2745b4c83ab058bd671c
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-01-28 16:09:55 -0600

    Fixed bugs in index.ts

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ const port = process.env.PORT || 6000;
 app.use(express.json());
 //app.use("/api/contacts", require("./src/routes/contactRoutes"))
 
-app.use("/api/users", require("./src/routes/userRoutes"));
+//app.use("/api/users", require("./src/routes/userRoutes"));
 
 app.use(errorHandler);
 app.use(cors(corsOptions)) 
