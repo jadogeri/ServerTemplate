a3af7d67a7b84d832cac962bf89246654e88100d
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-01-28 14:35:25 -0600

    Updated index.ts

--- index.ts
+++ index.ts
@@ -3,41 +3,30 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express,{ Request, Response } from 'express';
-// const connectDb = require("./configs/dbConnection");
-// const errorHandler = require("./middleware/errorHandler");
-// const {corsOptions} = require("./configs/cors")
-// const cors = require("cors");
-// const swaggerUi = require('swagger-ui-express')
-// const swaggerjsdoc = require("swagger-jsdoc")
-// import { swaggeroptions } from './swagger-jsdocs';
-//const swaggerFile = require('./swagger_output.json')
+//const connectDb = require("./configs/dbConnection");
+const errorHandler = require("./src/middlewares/errorHandler");
+const {corsOptions} = require("./src/configs/cors")
+const cors = require("cors");
 
 
 //connectDb();
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 6000;
 
 
-//const spacs = swaggerjsdoc(swaggeroptions);
 app.use(express.json());
-//app.use("/api/contacts", require("./routes/contactRoutes"))
+//app.use("/api/contacts", require("./src/routes/contactRoutes"))
 
-//app.use("/api/users", require("./routes/userRoutes"));
-//app.use("/api/auths", require("./routes/authRoutes"));
+app.use("/api/users", require("./src/routes/userRoutes"));
 
-// app.use(errorHandler);
-// app.use(cors(corsOptions)) 
+app.use(errorHandler);
+app.use(cors(corsOptions)) 
 
 
 app.get('/', (req: Request, res : Response) => {
   res.send({message:"home"});
 });
-
-//swagger autogen 
-//app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-//swagger jsdocs
-//app.use("/api-docs", swaggerUi.serve,swaggerUi.setup(spacs))
 
 
 if (process.env.NODE_ENV !== 'test') {
