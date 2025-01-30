d8891000297eddb62aa68c650db27d7754dd922e
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-01-30 12:34:15 -0600

    Added loginUser.ts

---
+++ src/controllers/userController/loginUser.ts
@@ -0,0 +1,84 @@
+/**
+ * @author Joseph Adogeri
+ * @version 1.0
+ * @since 18-JAN-2025
+ *
+ */
+
+const asyncHandler = require("express-async-handler");
+import * as bcrypt from "bcrypt";
+import { Response, Request } from 'express';
+import { IUser } from '../../interfaces/IUser';
+import * as  jwt from "jsonwebtoken";
+import { IAuth } from "../../interfaces/IAuth";
+import * as userService from"../../services/userService"
+import * as authService from"../../services/authService"
+import { errorBroadcaster } from "../../utils/errorBroadcaster";
+import { isValidEmail, isValidPassword } from "../../utils/inputValidation";
+
+
+/**
+*@desc Login user
+*@route POST /api/users/login
+*@access public
+*/
+
+
+export const loginUser = asyncHandler(async (req : Request, res: Response)  => {
+ // #swagger.tags = ['User'] // Tag this route with 'Users'
+    // #swagger.summary = 'Get all users'
+    
+    try{
+  const { email, password } : IUser = req.body;
+  console.log(email,password)
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  if(!isValidEmail(email as string)){
+    errorBroadcaster(res,400,"not a valid email")
+
+  }
+  if(!isValidPassword(password as string)){
+    errorBroadcaster(res,400,"not a valid password")
+  }  
+  const user  = await userService.getByEmail(email as string);
+    
+    //compare password with hashedpassword 
+  if (user &&  await bcrypt.compare(password,user.password as string)) {
+
+    let payload = {
+      user: {
+        username: user.username as string , email: user.email as string , id: user._id ,
+      },
+    }
+    //post fix operator   knowing value cant be undefined
+    let secretKey  = process.env.JSON_WEB_TOKEN_SECRET! ;
+    const accessToken  =  jwt.sign( payload,secretKey as jwt.Secret,  { expiresIn: "30m" } );
+    //add token and id to auth 
+    const authUser : IAuth = {
+      id : user._id,
+      token : accessToken
+    }
+
+    const authenticatedUser = await authService.getById(user._id);
+    if(!authenticatedUser){
+      await authService.create(authUser);
+    }else{
+      await authService.update(authUser);;     
+    }
+      res.status(200).json({ accessToken }); 
+  }else{ 
+    res.status(400).json({ message: "email or password is correct" });
+  }
+
+}catch(e){
+  console.log(e)
+}
+
+});
+  
+
+
+
+
