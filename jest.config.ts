409f9a38683cf3a0e9735a3800374a563eaa8126
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-10-17 15:46:26 -0500

    updated test scripts for v1 and v2

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,12 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    setupFiles: ['./tests/v1/setupTests.ts'],
+    setupFiles: ['./tests/v1/setupTests.ts',// './tests/v2/setupTests.ts'
+
+    ],
+    testMatch: [
+      '<rootDir>/tests/v1/**/*.test.ts',
+      '<rootDir>/tests/v2/**/*.test.ts'
+    ],
 
   };
