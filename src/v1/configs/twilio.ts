e5e8c0045589b3f07fe776ce85fa534ce2dbdeea
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-10-17 15:14:27 -0500

    remove comments in twilio.ts

--- src/v1/configs/twilio.ts
+++ src/v1/configs/twilio.ts
@@ -5,6 +5,6 @@ const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER 
 
-// const twilioClient = Twilio(twilioAccountSid, twilioAuthToken);
-// export {twilioAccountSid, twilioAuthToken, twilioPhoneNumber, twilioClient}
+const twilioClient = Twilio(twilioAccountSid, twilioAuthToken);
+export {twilioAccountSid, twilioAuthToken, twilioPhoneNumber, twilioClient}
 
