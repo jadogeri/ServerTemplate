15ccb1b7d0f297705f0912ae6e7e0fd96cb4ac81
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-10-17 22:02:10 -0500

    Added UserModel.ts

---
+++ src/v2/models/UserModel.ts
@@ -0,0 +1,22 @@
+import mongoose, { Model } from 'mongoose';
+import { UserSchema } from '../schemas/UserSchema';
+import { IUser } from '../interfaces/IUser';
+
+
+export class UserModel{
+
+    private schema: UserSchema;
+
+    constructor(){
+        this.schema = new UserSchema();
+    }
+
+    public getInstance():  Model<IUser>{
+
+        const userSchema = this.schema.getInstance();   
+        
+        const User: Model<IUser>  = mongoose.model<IUser>("User", userSchema);
+
+        return User;
+    }
+}
